2a0b2a711c47c3aef28c48abceb7d4120277ab22
olavbr <olavbr@nr.no>
2019-07-30 11:32:32 +0200

    small change SNCGP.R

--- scripts/SNCGP.R
+++ scripts/SNCGP.R
@@ -6,7 +6,7 @@ set.seed(123)
 #Simulate data-------------------------------------
 par = list()
 par$theta = 0.0005;par$beta = 0.05; par$alpha = 1.2; par$lambda = 1;w.delta = 0.1;r_x =1
-GMT = simFiniteGmeasure(par,w.delta = w.delta, r_x = r_x,minGamma = 1)
+GMT = simFiniteGmeasure(par,w.delta = w.delta, r_x = r_x,minGamma = 5)
 #--------------------------------------------------
 
 #Plot simulation-----------------------------------
@@ -26,7 +26,7 @@ for(i in 1:r_x){
 #run MCMC------------------------------------------
 x11()
 x = GMT$x
-conf = setConf(R = 2000,kappaVonMises=0,starAtTruth = FALSE, lBext = 1.2,jumpFactor = 0.1,splitFactor = 1,truncGamma = 1)
+conf = setConf(R = 2000,kappaVonMises=0,starAtTruth = FALSE, lBext = 1.2,jumpFactor = 0.1,splitFactor = 1,truncGamma = 5)
 run = run.MCMC.SNGCP(x,conf)
 #--------------------------------------------------
 
@@ -60,11 +60,6 @@ for(j in indeks){
 }
 
 conf = setConf(R = 5000,lBext = 1.2,truncGamma = 5)
+x11()
 run = run.MCMC.SNGCP(x = xReal,conf)
 reportRun(run = run)
-par(mfrow = c(3,3))
-r_x = length(xReal)
-for(i in 1:r_x){
-  plot(treesMod[[i]]$x,treesMod[[i]]$y)
-}
-
